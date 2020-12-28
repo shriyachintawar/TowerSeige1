f5e13a7fb45683458d6d5e80ab825a0647b7309d
shriyachintawar <74260103+shriyachintawar@users.noreply.github.com>
2020-12-28 20:21:09 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box{
+
+    constructor(x,y){
+        var options ={
+            'restitution':0.4
+           
+           
+        }
+        this.body = Bodies.rectangle(x,y,30,40,options);
+        this.width = 30;
+        this.height = 40;
+        
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        //angleMode(RADIANS);
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+       
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
